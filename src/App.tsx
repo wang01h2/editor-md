1d23859f551718aa7f462b0c6132027615e40b71
wang01h2 <1299839398@qq.com>
2023-02-18 20:03:07 +0800

    feat: demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
 import './App.css'
-import MyEditor from "./components/Editor"
+import '@toast-ui/editor/dist/toastui-editor.css';
+import {Editor} from "@toast-ui/react-editor";
+import React, {useRef} from "react";
+import ReactToPrint from 'react-to-print';
+import initContent from './initContent';
 
 function App() {
+  const editorRef = useRef(null);
+  let printRef: Element | null = useRef(null);
+  let initMdContent = initContent;
 
+  const setMdContent = () => {
+    printRef = editorRef.current.editorInst.preview.el;
+    window.localStorage.setItem('mdContent', printRef);
+  }
   return (
     <div>
-      <header>111</header>
-      <MyEditor></MyEditor>
+      <header className={'header px-3'}>
+        <span>hi～</span>
+        <ReactToPrint
+          trigger={() => <div className={'download-btn'}>
+            Download
+          </div>}
+          content={() => printRef}
+          onBeforeGetContent={() => setMdContent()}
+        />
+      </header>
+      <>
+        <Editor
+          previewStyle="vertical"
+          height="93vh"
+          initialEditType="markdown"
+          initialValue={initMdContent}
+          ref={editorRef}
+          onChange={() => setMdContent()}
+        />
+      </>
     </div>
   )
 }
---
+++ src/initContent.js
@@ -0,0 +1,25 @@
+export default `# 小明
+
+开发工程师
+
+
+## 技能
+
+- 后端: Node.js; Express; Nest.js
+- 数据库: MongoDB; MySql; Redis
+- 前端: React.js; Vue.js; JavaScript; CSS;
+
+## 项目
+
+- **[todo list](https://wang01h2.github.io/vue-todo-app/)**: 适配手机与PC端的 极简 todo list
+    - 使用Vue3 Vite开发与构建
+
+## 工作经历
+
+- **xxx公司**:  (2020/05 - now)
+  Trying to build something people want
+
+## 教育
+
+- xxxxxxx *(2014 – 2017)*
+`
